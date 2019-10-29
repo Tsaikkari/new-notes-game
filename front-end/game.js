b3eb4b39fe603bf340fc11ee1d11a0d2753257f2
Tsaikkari <trospe@gmx.de>
2019-10-29 01:40:12 +0100

    Remove userClickedKeys array

--- front-end/game.js
+++ front-end/game.js
@@ -1,6 +1,5 @@
 let startPoints = [];
 let gameNotes = [];
-let userClickedKeys = [];
 let keysAvailable = {'99': 'c', '100': 'd', '101': 'e', '102': 'f','103': 'g', '97': 'a', '98': 'b'};
 let level = 0;
 let started = false;
@@ -11,42 +10,38 @@ $(document).keydown(function() {
     var pos = 0;
     startPoint = this.startPoint; // note's starting point at the screen above corresponding keyboard key (button)
     var id = setInterval(frame, 10);
-      
+
     function frame() {
-      if (pos === 1077) { //1177 is at a moment a placeholder for staffPositions
+      if (pos === 1077) { //1177 is at the moment a placeholder for staffPositions
         clearInterval(id);
       } else {
         pos++;
         elem.style.top = pos + 'px';
       }
     }
-  nextNote();
-  started = true;
+    //nextNote();
+    started = true;
   }
 })
 
 userClickedKeys = (function(event) {
   let keycode = event.which || event.keyCode
   let userChosenKey = keysAvailable[keycode]
-  playSound(userChosenKey);
-  userClickedKeys.push(userChosenKey);
+  let nextNote = function(randomNote) {
+    var randomNumber = Math.floor(Math.random() * 7);
+    var randomNote = keysAvailable[randomNumber]
+    gameNotes.push(randomNote)
+        
+      if (randomNote === userChosenKey) {
+        playSound(userChosenKey);
+        nextNote();
+      } else {
+        playSound("wrong")
+      }
+  }
 })
 
 $(document).on('keydown', userClickedKeys);
-
-function nextNote(randomNote) {
-  userClickedKeys = [];
-  var randomNumber = Math.floor(Math.random() * 7);
-  var randomNote = notes[randomNumber]
-  gameNotes.push(randomNote)
-      
-    if (gameNotes === userClickedKeys) {
-      //goTo();
-      nextNote();
-    } else {
-      playSound("wrong")
-    }
-}
 
 function playSound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
