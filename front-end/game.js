1a57dc72412836b093475fe4ff14563a54afba8b
Tsaikkari <trospe@gmx.de>
2019-10-31 10:50:54 +0100

    Swap keyboard with staff and vice versa depending on which link is clicked

--- front-end/game.js
+++ front-end/game.js
@@ -4,13 +4,45 @@ let keysAvailable = {'99': 'c', '100': 'd', '101': 'e', '102': 'f','103': 'g', '
 let level = 0;
 let started = false;
 
-// Hides the intro when a link is clicked
 $(".nav-link").on("click", function() {
   $("#intro").hide();
 })
-// Makes the intro reappear
+
 $("#menu").on("click", function() {
   $("#intro").show();
+})
+
+function swapElement(a, b) {
+  // create a temporary marker div
+  var aNext = $('<div>').insertAfter(a);
+  a.insertAfter(b);
+  b.insertBefore(aNext);
+  // remove marker div
+  aNext.remove();
+}
+
+$('.level').on('click', function () {
+  var a = $('#klavier' + $('#a').val());
+  var b =  $(!'#staff' + $(!'#b').val());
+    if (b = $(!'#klavier' + $('#b').val()) && (a = $('#staff' + $('#b').val()))) {
+    swapElement(a, b);
+  }
+})
+
+$('.test').on('click', function () {
+  var a = $('#klavier' + $('#a').val());
+  var b = $('#staff' + $('#b').val());
+  swapElement(a, b);
+})
+
+$('.test').on('click', function () {
+  var a = $('#staff' + $('#a').val());
+  var b = $('#klavier' + $('#b').val());
+  $('.level').on('click', function () {
+    var a = $('#staff' + $('#a').val());
+    var b = $('#klavier' + $('#b').val());
+    swapElement(a, b);
+  })
 })
 
 $(document).keydown(function() {
@@ -18,41 +50,39 @@ $(document).keydown(function() {
       var elem = document.getElementById("note");
       var pos = 0;
       startPoint = this.startPoint; // note's starting point at the screen above corresponding keyboard key (button)
-      var id = setInterval(frame, 10);
+      var id = setInterval(frame, 20);
 
       function frame() {
-        if (pos === 1077) { //1177 is at the moment a placeholder for staffPositions
+        if (pos === 513) { //513 is a placeholder for staffPositions
           clearInterval(id);
         } else {
           pos++;
           elem.style.top = pos + 'px';
         }
       }
-      
       started = true;
-    }
-
-
-userClickedKeys = (function(event) {
-  let keycode = event.which || event.keyCode
-  let userChosenKey = keysAvailable[keycode]
-  let nextNote = function(randomNote) {
-    var randomNumber = Math.floor(Math.random() * 7);
-    var randomNote = keysAvailable[randomNumber]
-    gameNotes.push(randomNote)
-        
-      if (randomNote === userChosenKey) {
-        playSound(userChosenKey);
-        nextNote();
-      } else {
-        playSound("wrong")
-      }
+      level != 0;
+     
+      let userClickedKeys = (function(event) {
+        let keycode = event.which || event.keyCode
+        let userChosenKey = keysAvailable[keycode]
+        let nextNote = function(randomNote) {
+          var randomNumber = Math.floor(Math.random() * 7);
+          var randomNote = keysAvailable[randomNumber]
+          gameNotes.push(randomNote)
+              
+            if (randomNote === userChosenKey) {
+              playSound(userChosenKey);
+              nextNote();
+            } else {
+              playSound("wrong")
+            }
+        }
+      })
+      $(document).on('keydown', userClickedKeys);
   }
 })
 
-
-$(document).on('keydown', userClickedKeys);
-})
 function playSound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
