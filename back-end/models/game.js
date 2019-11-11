30d5a6be78582c9c5f2716d8f8b87cc2d1483c98
Tsaikkari <trospe@gmx.de>
2019-11-11 11:30:19 +0100

    Add games prop to user model

--- back-end/models/game.js
+++ back-end/models/game.js
@@ -1,9 +1,10 @@
 module.exports = class Game {
-    constructor(name, levels = [], tests = [], players = [], id) {
+    constructor(name, levels = [], tests = [], players = [], games = [], id) {
         this.name = name
         this.levels = levels
         this.tests = tests
         this.players = players
+        this.games = games
         this.id = id
     }
 
@@ -11,7 +12,7 @@ module.exports = class Game {
         console.log((this.name), 'number of players', this.players.length)
     }
 
-    static create({ name, levels, tests, players, id }) {
-        return new Game(name, levels, tests, players, id)
+    static create({ name, levels, tests, players, games, id }) {
+        return new Game(name, levels, tests, players, games, id)
     }
 }
