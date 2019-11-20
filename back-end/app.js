530f475086e9aaf4b7a0071a40e64c137fff03db
Tsaikkari <trospe@gmx.de>
2019-11-20 21:07:06 +0100

    Add app.js and test files

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRouter = require('./routes/user')
+const gameRouter = require('./routes/game')
+
+require('./mongo-connection')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(bodyParser.json())
+
+app.use('/user', userRouter)
+app.use('/game', gameRouter)
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+module.exports = app
--- back-end/index.js
+++ back-end/index.js
@@ -1,22 +1,4 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const userRouter = require('./routes/user')
-const gameRouter = require('./routes/game')
-
-require('./mongo-connection')
-
-const app = express()
-
-app.set('view engine', 'pug')
-app.use(bodyParser.json())
-
-app.use('/user', userRouter)
-app.use('/game', gameRouter)
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+const app = require('./app')
 
 app.listen(3000, () => {
   console.log('Server listening')
