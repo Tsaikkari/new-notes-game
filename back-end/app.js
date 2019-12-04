bb0e205ddf9374f392f047f46370ed6af4ff1e3d
Tsaikkari <trospe@gmx.de>
2019-12-04 13:09:22 +0100

    Add back-end folder

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 const userRouter = require('./routes/user')
 const gameRouter = require('./routes/game')
 const staffRouter = require('./routes/staff')
@@ -9,6 +9,7 @@ const noteRouter = require('./routes/note')
 require('./mongo-connection')
 
 const app = express()
+app.use(cors())
 
 app.set('view engine', 'pug')
 app.use(bodyParser.json())
