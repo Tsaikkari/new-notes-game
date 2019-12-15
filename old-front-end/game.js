20559fd135381265813ed1ef85e20ac461c5c879
Tsaikkari <trospe@gmx.de>
2019-12-15 16:12:30 +0100

    Change methods

--- old-front-end/game.js
+++ old-front-end/game.js
@@ -1,6 +1,5 @@
 let keysAvailable = [65, 83, 68, 70, 71, 72, 74];
-let staffPositions = [550, 537, 524, 510, 497, 483, 470];
-let notes = [];
+notes = [];
 let gameNotes = [];
 let userChosenKeys = [];
 let userChosenButtons = [];
@@ -37,102 +36,95 @@ $("button").click(function() {
 // Random note starts dropping down when user starts the game
 $(document).keydown(function() {
   if (!started) {
-    randomNote();
+    createRandomNote();
     started = true; 
-    for (let i = 0; i < notes.length; i++) {
-      var elem = document.getElementById([notes[i].id]);
-      var pos = 0;
-      var id = setInterval(frame, 37);
+    var elem = document.getElementById("note");
+    var pos = 0;
+    var id = setInterval(frame, 10);
 
-      function frame() {
-        let staffPosition = this.staffPosition;
-        if (pos === staffPosition) { 
-          clearInterval(id);
-        } else {
-          pos++;
-          elem.style.top = pos + 'px';
-        }
+    function frame() {
+      let staffPosition = this.staffPosition;
+      if (pos === staffPosition) { 
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + 'px';
       }
     }
   }
-  let userChosenKeys = (function(event) {
-  let keycode = event.which || event.keyCode;
-  let userChosenKey = keysAvailable[keycode];
-  userChosenKeys.push(userChosenKey);
-  playSound(userChosenKey);
-  check(userChosenKey);
-  })
+  check();
 })
+    
+function nextNote() {
+  document.addEventListener("keydown", function(event) {
+    for (let i = 0; i < keysAvailable.length; i++) {
+      let keycode = event.which || event.keyCode;
+      let userChosenKey = keysAvailable[keycode];
+      userChosenKeys.push(userChosenKey);
+      playSound(userChosenKey);
+      check(userChosenKey);
+      randomNotes = [];
+    }
+  })
+}
 
-function randomNote() {
+function createRandomNote() {
   for (let i = 0; i < keyboardKeys.length; ++i) {
     let elem = document.getElementById(keyboardKeys[i].id);
-  
+ 
     function createNoteAboveKey(elem, html) {
+      // Put a note above a key
       let note = document.createElement('h6');
-      note.setAttribute("id", ""); 
+      note.setAttribute("id", "note");
       note.style.cssText = "position:absolute; font-size: 3em; font-family: 'Raleway', sans-serif;";
-  
+      
       let coords = elem.getBoundingClientRect();
       note.style.left = keyboardKeys[i].left + "px";
       note.style.top = 5 + "px";
       note.innerHTML = html;
-      notes.push(note)
-      let randomNote = notes[Math.floor(Math.random() * 7)]
-      if (randomNote === notes[0]) {
-        note.setAttribute("id", "1")
-        note = "c";
-        staffPosition = staffPositions[0];
-      } else if (randomNote === notes[1]) {
-        note.setAttribute("id", "2")
-        note = "d";
-        staffPosition = staffPositions[1];
-      } else if (randomNote === notes[2]) {
-        note.setAttribute("id", "3")
-        note = "e";
-        staffPosition = staffPositions[2];
-      } else if (randomNote === notes[3]) {
-        note.setAttribute("id", "4")
-        note = "f";
-        staffPosition = staffPositions[3];
-      } else if (randomNote === notes[4]) {
-        note.setAttribute("id", "5")
-        note = "g";
-        staffPosition = staffPositions[4];
-      } else if (randomNote === notes[5]) {
-        note.setAttribute("id", "6")
-        note = "a";
-        staffPosition = staffPositions[5];
-      } else if (randomNote === notes[6]) {
-        note.setAttribute("id", "7")
-        note = "b";
-        staffPosition = staffPositions[6];
-      }
+      let startPoint = note.style.left;
+      notes.push(note);
+      randomNote = notes[Math.floor(Math.random() * 7)]
+
+      // Define note's stopping position
+      /*if (startPoint === keyboardKeys[0].left + "px") {
+        staffPosition = keyboardKeys[0].top + "px";
+      } else if (startPoint === keyboardKeys[1].left + "px") {
+        staffPosition = keyboardKeys[1].top + "px";
+      } else if (startPoint === keyboardKeys[2].left + "px") {
+        staffPosition = keyboardKeys[2].top + "px";
+      } else if (startPoint === keyboardKeys[3].left + "px") {
+        staffPosition = keyboardKeys[3].top + "px";
+      } else if (startPoint === keyboardKeys[4].left + "px") {
+        staffPosition = keyboardKeys[4].top + "px";
+      } else if (startPoint === keyboardKeys[5].left + "px") {
+        staffPosition = keyboardKeys[5].top + "px";
+      } else if (startPoint === keyboardKeys[6].left + "px") {
+        staffPosition = keyboardKeys[6].top + "px";
+      }*/
       
+      gameNotes.push(randomNote)
       return randomNote;
+      
     }
     randomNote = createNoteAboveKey(elem, 'o');
     document.body.append(randomNote);
-    gameNotes.push(randomNote);
     console.log(randomNote)
-    playSound(randomNote);
+    playSound();
+    nextNote();   
   }
 }
 
 function check() {
   let userChosenKey = this.userChosenKey;
-  if (randomNote === userChosenKey) {
-    nextNote();
+  if (randomNote == userChosenKey) {
+    playSound(userChosenKey);
+    createRandomNote();
   } else {
     playSound("wrong")
     startOver();
   }
 }
-
-/*function playSound(name){
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}*/
 
 // Add event listener to piano keyboard keys
 let numberOfKeys = document.querySelectorAll(".key").length;
@@ -152,38 +144,38 @@ function playSound(key) {
     case "c":
       let c = new Audio("sounds/c.mp3");
       c.play();
-      break;
-      case "d":
-        let d = new Audio("sounds/d.mp3");
-        d.play();
-      break;
-      case "e":
-        let e = new Audio("sounds/e.mp3");
-        e.play();
-      break;
-      case "f":
-        let f = new Audio("sounds/f.mp3");
-        f.play();
-      break;
-      case "g":
-        let g = new Audio("sounds/g.mp3");
-        g.play();
-      break;
-      case "a":
-        let a = new Audio("sounds/a.mp3");
-        a.play();
-      break;
-      case "b":
-        let b = new Audio("sounds/b.mp3");
-        b.play();
-      break;
-      case "":
-        let wrong = new Audio("sounds/wrong.mp3")
-        wrong.play();
-      break;
-      default: console.log(key)
-    }
+    break;
+    case "d":
+      let d = new Audio("sounds/d.mp3");
+      d.play();
+    break;
+    case "e":
+      let e = new Audio("sounds/e.mp3");
+      e.play();
+    break;
+    case "f":
+      let f = new Audio("sounds/f.mp3");
+      f.play();
+    break;
+    case "g":
+      let g = new Audio("sounds/g.mp3");
+      g.play();
+    break;
+    case "a":
+      let a = new Audio("sounds/a.mp3");
+      a.play();
+    break;
+    case "b":
+      let b = new Audio("sounds/b.mp3");
+      b.play();
+    break;
+    case "":
+      let wrong = new Audio("sounds/wrong.mp3")
+      wrong.play();
+    break;
+    default: console.log(key)
   }
+}
 
 function startOver() {
   level = 0
--- old-front-end/keyboardKey.js
+++ old-front-end/keyboardKey.js
@@ -1,31 +1,38 @@
 // keyboard key-buttons
-let keyboardKeys = [
+let keyboardKeys = [  
     { 
       id: "c", 
-      left: 349 
+      left: 349,
+      top: 550 
     },
     {
       id: "d", 
-      left: 440 
+      left: 440,
+      top: 537 
     },
     {
       id: "e", 
-      left: 525 
-    },
+      left: 525,
+      top: 524 
+    }, 
     {
       id: "f", 
-      left: 615
+      left: 615,
+      top: 510
     },
     {
       id: "g", 
-      left: 707 
+      left: 707,
+      top: 497 
     },
     {
       id: "a", 
-      left: 794 
+      left: 794,
+      top: 483 
     },
     {
       id: "b", 
-      left: 880     
+      left: 880,
+      top: 470     
     }
-    ]
+]
