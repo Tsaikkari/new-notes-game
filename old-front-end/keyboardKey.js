e981370b2c8d9ff50cb97e5b3f97e7a9dae20736
Tsaikkari <trospe@gmx.de>
2019-12-21 20:32:20 +0100

    Change values of staffpositions

--- old-front-end/keyboardKey.js
+++ old-front-end/keyboardKey.js
@@ -3,36 +3,36 @@ let keyboardKeys = [
     { 
       id: "c", 
       left: 27,
-      top: 128
+      top: 93
     },
     {
       id: "d", 
       left: 35,
-      top: 125
+      top: 91
     },
     {
       id: "e", 
       left: 42,
-      top: 122
+      top: 88
     }, 
     {
       id: "f", 
       left: 49,
-      top: 119
+      top: 86
     },
     {
       id: "g", 
       left: 56,
-      top: 116
+      top: 84
     },
     {
       id: "a", 
       left: 63,
-      top: 113
+      top: 81
     },
     {
       id: "b", 
       left: 70,
-      top: 110
+      top: 79
     }
 ]
