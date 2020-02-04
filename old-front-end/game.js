4345347d37aba5e5a886c2cd517e59ac3b311e1f
Tsaikkari <trospe@gmx.de>
2020-02-04 19:18:13 +0100

    Changing functions

--- old-front-end/game.js
+++ old-front-end/game.js
@@ -1,101 +1,81 @@
-let keysAvailable = ["c", "d", "e", "f", "g", "a", "b"];
+let keybind = ["C", "D", "E", "F", "G", "A", "B"];
+let notes = ["c", "d", "e", "f", "g", "a", "b"];
 let level = 0;
 let started = false;
 
-// Starting the game
-$(document).keydown(function () {
+$(document).keydown(function (e) {
   if (!started)
-  createNote();
+  getNote();
   started = true;
-})
+  let pressed = String.fromCharCode(e.which);
+  let index = keybind.indexOf(pressed);
+  $('.key[data-note="' + notes[index] + '"]').mousedown();
+  setTimeout(function () {
+    $(".key").removeClass("active").unbind("mouseover");
+  }, 200);
+});
 
-function popRandomNote() {
-  //localStorage.removeItem('learn');
-  $('button').click(function () {
-    // e.preventdefault();
-    // check if its learn or exam 
-    // let isLearn = localStorage.getItem('learn') ==="true";
-    // if(isLearn) return ;
-    let notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    let randomNotes = [];
-    let randomNote = notes[Math.floor(Math.random() * notes.length)];
-    randomNotes.push(randomNote);
-    let userClickedButton = $('.' + randomNote + '-test-note');
-    if (userClickedButton){
-      $(userClickedButton).find('span').removeClass('hidden');
-    }
-    return [randomNote];
-  })
+let note_element = $(".keyboard-container .note")
+function getNote() {
+  let note = "";
+  note = notes[note];
+  note_element.addClass(note + '-note');
+  return [note];
 }
 
-// Add event listener to detect which key is pressed
-$(document).keydown(function (event) {
-  //localStorage.setItem('learn', true);
-  let userChosenKeys = [];
-  let key = event.keyCode || event.key;
-  for (let i = 0; i < keysAvailable.length; i++) { 
-    let userChosenKey;
-    if (key === 67) {
-      userChosenKey = keysAvailable[0];
-    } else if (key === 68) {
-      userChosenKey = keysAvailable[1];
-    } else if (key === 69) {
-      userChosenKey = keysAvailable[2];
-    } else if (key === 70) {
-      userChosenKey = keysAvailable[3];
-    } else if (key === 71) {
-      userChosenKey = keysAvailable[4];
-    } else if (key === 65) {
-      userChosenKey = keysAvailable[5];
-    } else if (key === 66) {
-      userChosenKey = keysAvailable[6];
-    }
-    if (userChosenKey) {
-      userChosenKeys.push(userChosenKey);
-      // hide the existing nodes
-      $('.note-list > h6').each(function () {
-        $(this).css('display', 'none');
-      })
-      $('#keyboard > button').each(function () {
-        $(this).css('background-color', '');
-        $(this).addClass("clicked-key");
-      })
-
-      let chosenElem = $('.' + userChosenKey + '-note');
-      let chosenKeyboardKey = $('#' + userChosenKey);
-      chosenKeyboardKey.text(event.key);
-      setTimeout(function () {
-        chosenKeyboardKey.removeClass("clicked-key");
-      }, 1500); // TODO: duration of sound needs to match
-      if (chosenKeyboardKey) chosenKeyboardKey.css('background-color', '#4897d8');
-      if (chosenElem) chosenElem.css('display', 'block');
-      playSound(userChosenKey);
-      console.log(userChosenKey)
-    } else {
-    gameover()
-    }
+function getRandomNote() {
+  let note = "";
+  let created = false;
+  let randomKey = Math.round(Math.random() * 6);
+  let randomNote = notes[randomKey];
+  note = randomNote;
+  if (!created) {
+    note_element.addClass(randomNote + '-note');
   }
-})
+  return [note];
+}
+
+function removeNote(guessedNote) {
+  note_element.removeClass(guessedNote[0] + "-note");
+}
 
 function ambigNote(taskNote) {
   return taskNote;
 }
 
 // Check if the user played right or wrong
-let randomNoteInfo = popRandomNote();
-//let randomNote = ambigNote(randomNoteInfo[0]);
-function checkUserChoise(randomNote, userClickedButton, userChosenButton) {
-  let buttonClicked = userClickedButton;
-  if (buttonClicked == randomNote) {
-    if (randomNote === userChosenButton + "-note") {
-      console.log("Good")
-    }
+let randomNoteInfo = getRandomNote();
+let randomNote = ambigNote(randomNoteInfo[0]);
+
+function checkUserChoise(randomNote, clickedNote) {
+  let noteClicked = clickedNote;
+  if (noteClicked == randomNote) {
+   console.log("Good");
   } else {
     gameover();
   }
-  /*if (randomNotes[currentTone] === userChosenButtons[currentTone]) {
-  }*/
 }
+
+$(".key").mousedown(function () {
+  let $this = $(this);
+  let clickedNote = $(this).attr("data-note");
+  playSound($this, clickedNote);
+  //checkUserChoise(note, clickedNote);
+  //removeNote(note);
+  noteInfo = getNote();
+  note = noteInfo[0];
+
+  $(".key").mouseover(function () {
+    let $this = $(this);
+    let clickedNote = $(this).attr("data-note");
+    playSound($this, clickedNote);
+    //checkUserChoise(note, clickedNote);
+});
+
+  $(document).mouseup(function () {
+      $(".key").unbind("mouseover");
+  });
+});
 
 // game over
 function gameover() {
@@ -105,55 +85,18 @@ function gameover() {
     $("body").removeClass("game-over");
   }, 200);
   $("#line").text("Game Over").addClass("game-over");
-  startOver();
 }
 
-// Create a note on the fly
-function createNote() {
-  let notes = [];
-  for (let i = 0; i < keyboardKeys.length; ++i) {
-    let elem = document.getElementById(keyboardKeys[i].id);
-    function createNoteAboveKey(elem, html) {
-    let note = document.createElement('h6');
-    note.setAttribute("id", "note");
-    note.style.cssText = "position:absolute; font-size: 3em; font-family: 'Raleway', sans-serif; display:none;";
-    note.style.left = keyboardKeys[i].left + "%";
-    note.style.top = keyboardKeys[i].top + "%";
-    note.setAttribute("class", keyboardKeys[i].class);
-    note.innerHTML = html;
-    return note;
-    }
-    let note = createNoteAboveKey(elem, "o");
-    // adding new notes to the list
-    $('.note-list').append(note);
-    notes.push(note);
-  }
-}
-
-// Add event listener to piano keyboard keys
-$('button').click(function () {
-  //let isLearn = localStorage.getItem('learn') === "true";
-  //if (isLearn) return;
-
-  // user should be able to see random notes
-  popRandomNote();
-  let userChosenButtons = [];
-  let userChosenButton = $(this).attr("id");
-  userChosenButtons.push(userChosenButton);
-  playSound(userChosenButton);
-})
-
-function playSound(name) {
+function playSound($this, name) {
   let audio = new Audio("sounds/" + name + ".mp3");
+  $this.addClass("active");
+  console.log(name)
   audio.play();
 }
 
-function startOver() {
-  level = 0;
-  randomNotes = [];
-  notes = [];
-  started = false;
-}
+
+
+// This code is inspired by a React-guy in Berlin
 
 
 
