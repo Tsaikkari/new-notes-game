8b0800c7d8aa1f8653998fcc89778f8514ae7f70
Tsaikkari <trospe@gmx.de>
2020-02-07 23:13:55 +0100

    removeBlackKeyNote()

--- old-front-end/game.js
+++ old-front-end/game.js
@@ -1,6 +1,5 @@
 let notes = ['B', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'C'];
 let keybind = ['S', 'C', 'K', 'D', 'L', 'E', 'F', 'M', 'G', 'N', 'A','O', 'B', 'Z'];
-let accidentals = ["", "", " sharp", " flat"];
 
 $(function () {
   setup();
@@ -19,42 +18,33 @@ $(function () {
       let index = keybind.indexOf(pressed);
       $('.key[data-note="' + notes[index] + '"]').mousedown();
       setTimeout(function () {
-          $(".key").removeClass("active").unbind("mouseover");
+        $(".key").removeClass("active").unbind("mouseover");
       }, 200);
     }
   });
 
+  notes.splice(0, 8);
   let note_element = $(".note_sheet.treble .note").children("span");
-  let note_accidental = note_element.find("span");
+
   function getNote() {
     let note = "";
     let created = false;
     let random_key = Math.round(Math.random() * 6);
-    let random_acc = Math.round(Math.random() * 3);
     let onlyNote = notes[random_key];
-    let onlyAcc = accidentals[random_acc];
-    if (notes.indexOf(onlyNote) < 3 && onlyAcc == " flat") {
-      onlyAcc = accidentals[Math.round(Math.random() * 2)];
-    }
-    if (onlyNote == onlyAcc) {
-      $(note_element).ignoreCase;
-    }
-    note = onlyNote + onlyAcc;
+    
+    note = onlyNote;
     if (!created) {
       note_element.addClass('note-' + onlyNote);
-      note_accidental.addClass(onlyAcc);
     }
+    removeBlackKeyNote(note);
     console.log(note)
     return [note];
   }
-
-  function removeNote(guessedNote) {
-    if (guessedNote[1] == 3) {
-      note_element.removeClass('note-' + guessedNote[0] + guessedNote[1] + "-treble");
-      note_accidental.removeClass(guessedNote.slice(3));
-    } else {
-      note_element.removeClass('note-' + guessedNote[0] + guessedNote[1]);
-      note_accidental.removeClass(guessedNote.slice(3));
+  // TODO: Fix
+  function removeBlackKeyNote() {
+    let black_key_note = $(note_element).attr("note-" + "data-note" + "#");
+    if (black_key_note) {
+      note_element.removeClass('note');
     }
   }
 
@@ -63,7 +53,7 @@ $(function () {
   }
 
   let noteInfo = getNote();
-  let note = ambigNote(noteInfo[0]);
+  note = ambigNote(noteInfo[0]);
   // Check if the user played right or wrong
   function checkUserChoise(note, clickedNote) {
     let noteClicked = clickedNote;
@@ -80,7 +70,6 @@ $(function () {
     let clickedNote = $(this).attr("data-note");
     play($this, clickedNote);
     checkUserChoise(note, clickedNote);
-    //removeNote(note);
     noteInfo = getNote();
     note = noteInfo[0];
 
@@ -100,7 +89,7 @@ $(function () {
 
 function setup() {
   $.each(notes, function (i, note) {
-    let $key = $('<button class="key" data-bind="' + keybind[i] + '" data-note=' + note/* + ' data-octave=' + octave*/ + '></button>');
+    let $key = $('<button class="key" data-bind="' + keybind[i] + '" data-note=' + note + '></button>');
     $(".keyboard").append($key);
   });
 }
