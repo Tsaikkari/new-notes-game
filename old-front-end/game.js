8ab8a77181db54623d9abb0bca2a68c3cce34ce2
Tsaikkari <trospe@gmx.de>
2020-02-07 15:59:18 +0100

    Adding accidentals

--- old-front-end/game.js
+++ old-front-end/game.js
@@ -1,85 +1,112 @@
-let keybind = ["C", "D", "E", "F", "G", "A", "B"];
-let notes = ["c", "d", "e", "f", "g", "a", "b"];
+let notes = ['B', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'C'];
+let keybind = ['S', 'C', 'K', 'D', 'L', 'E', 'F', 'M', 'G', 'N', 'A','O', 'B', 'Z'];
+let accidentals = ["", "", " sharp", " flat"];
 
-$(document).keydown(function (e) {
-  let pressed = String.fromCharCode(e.which);
-  let index = keybind.indexOf(pressed);
-  $('.key[data-note="' + notes[index] + '"]').mousedown();
-  setTimeout(function () {
-    $(".key").removeClass("active").unbind("mouseover");
-  }, 200);
-});
-
-let note_element = $(".keyboard-container .note .key");
-function getNote() {
-  let note = "";
-  let created = false;
-  let randomKey = Math.round(Math.random() * 6);
-  randomNote = notes[randomKey];
-  note = randomNote;
-  if (!created) {
-    note_element.addClass(note + 'test-note');
-  }
-  console.log(note)
-  return [note];
-}
-
-function removeNote(guessedNote) {
-  note_element.removeClass(guessedNote[0] + "-test-note");
-}
-
-function ambigNote(taskNote) {
-  return taskNote;
-}
-
-let noteInfo = getNote();
-let note = ambigNote(noteInfo[0]);
-// Check if the user played right or wrong
-function checkUserChoise(note, clickedNote) {
-  let noteClicked = clickedNote;
-  if (noteClicked == note) {
-   console.log("Good");
-  } else {
-    gameover();
-  }
-}
-
-function showNote(clickedNote, note) {
-  $('#keyboard > span').each(function () {
-    clickedNote = $('.' + note + 'test-note');
-    if (clickedNote) {
-      $(note).find('span').removeClass('hidden');
+$(function () {
+  setup();
+  $('input[type="radio"]').change(function () {
+    let checked = $('input[type="radio"]:checked').attr("id");
+    if (checked == "shownote") {
+      $("html").addClass("keybind");
+    } else {
+      $("html").removeClass("keybind");
     }
-  })
-  return randomNote;
-}
+  });
 
-$(".key").mousedown(function () {
-  let $this = $(this);
-  let clickedNote = $(this).attr("data-note");
-  playSound($this, clickedNote);
-  animateButton(clickedNote);
-  getNote();
-  showNote();
-  //checkUserChoise(note, clickedNote);
-  //removeNote(note);
-  noteInfo = getNote();
-  note = noteInfo[0];
+  $(document).keydown(function (e) {
+    let pressed = String.fromCharCode(e.which);
+    if (keybind.indexOf(pressed) !== -1) {
+      let index = keybind.indexOf(pressed);
+      $('.key[data-note="' + notes[index] + '"]').mousedown();
+      setTimeout(function () {
+          $(".key").removeClass("active").unbind("mouseover");
+      }, 200);
+    }
+  });
 
-  $(".key").mouseover(function () {
+  let note_element = $(".note_sheet.treble .note").children("span");
+  let note_accidental = note_element.find("span");
+  function getNote() {
+    let note = "";
+    let created = false;
+    let random_key = Math.round(Math.random() * 6);
+    let random_acc = Math.round(Math.random() * 3);
+    let onlyNote = notes[random_key];
+    let onlyAcc = accidentals[random_acc];
+    if (notes.indexOf(onlyNote) < 3 && onlyAcc == " flat") {
+      onlyAcc = accidentals[Math.round(Math.random() * 2)];
+    }
+    if (onlyNote == onlyAcc) {
+      $(note_element).ignoreCase;
+    }
+    note = onlyNote + onlyAcc;
+    if (!created) {
+      note_element.addClass('note-' + onlyNote);
+      note_accidental.addClass(onlyAcc);
+    }
+    console.log(note)
+    return [note];
+  }
+
+  function removeNote(guessedNote) {
+    if (guessedNote[1] == 3) {
+      note_element.removeClass('note-' + guessedNote[0] + guessedNote[1] + "-treble");
+      note_accidental.removeClass(guessedNote.slice(3));
+    } else {
+      note_element.removeClass('note-' + guessedNote[0] + guessedNote[1]);
+      note_accidental.removeClass(guessedNote.slice(3));
+    }
+  }
+
+  function ambigNote(taskNote) {
+    return taskNote;
+  }
+
+  let noteInfo = getNote();
+  let note = ambigNote(noteInfo[0]);
+  // Check if the user played right or wrong
+  function checkUserChoise(note, clickedNote) {
+    let noteClicked = clickedNote;
+    if (noteClicked == note) {
+    console.log("Good");
+    } else {
+      console.log("wrong");
+    }
+  }
+    
+  // Clicks
+  $(".key").mousedown(function () {
     let $this = $(this);
     let clickedNote = $(this).attr("data-note");
-    playSound($this, clickedNote);
-    //checkUserChoise(note, clickedNote);
-});
+    play($this, clickedNote);
+    checkUserChoise(note, clickedNote);
+    //removeNote(note);
+    noteInfo = getNote();
+    note = noteInfo[0];
 
-  $(document).mouseup(function () {
-      $(".key").unbind("mouseover");
+    $(".key").mouseover(function () {
+      let $this = $(this);
+      let clickedNote = $(this).attr("data-note");
+
+    play($this, clickedNote);
+    checkUserChoise(note, clickedNote);
+    });
+
+    $(document).mouseup(function () {
+        $(".key").unbind("mouseover");
+    });
   });
 });
 
+function setup() {
+  $.each(notes, function (i, note) {
+    let $key = $('<button class="key" data-bind="' + keybind[i] + '" data-note=' + note/* + ' data-octave=' + octave*/ + '></button>');
+    $(".keyboard").append($key);
+  });
+}
+
 function animateButton(clickedNote) {
-  $('#keyboard > button').each(function () {
+  $('.keyboard > .key').each(function () {
     $(this).css({'background-color': '', 'box-shadow': ''});
     $(this).addClass("clicked-key");
   })
@@ -93,7 +120,7 @@ function animateButton(clickedNote) {
 
 // game over
 function gameover() {
-  playSound('wrong') 
+  play('wrong') 
     $("body").addClass("game-over");
   setTimeout(function () {
     $("body").removeClass("game-over");
@@ -101,17 +128,12 @@ function gameover() {
   $("#line").text("Game Over").addClass("game-over");
 }
 
-function playSound($this, name) {
-  let audio = new Audio("sounds/" + name + ".mp3");
-  $this.addClass("active");
-  console.log(name)
-  audio.play();
+function play($this, name) {
+let audio = new Audio("sounds/" + name + ".mp3");
+$this.addClass("active");
+console.log(name)
+audio.play();
 }
-
-
-
-// This code is inspired by a React-guy in Berlin
-
 
 
 
